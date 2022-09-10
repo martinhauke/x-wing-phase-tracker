80443f16a872717289faf3f139b2259abb9a151a
Martin Hauke <martin.hauke@sandstorm.de>
2022-09-10 12:36:06 +0200

    Add basic functionality (cycle through phases)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './Header'
+
+enum Phase {
+  Planning,
+  System,
+  Activation,
+  Engagement,
+  End,
+}
 
 function App() {
+  const [currentPhase, setCurrentPhase] = useState<Phase>(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <section>
+        {currentPhase}
+        <button onClick={e => setCurrentPhase(prev => (prev + 1) % 5)}>Next</button>
+      </section>
     </div>
   );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,8 @@
+import React, { memo } from 'react'
+
+const Header = () => {
+  return <header><h1>X-Wing Phase Tracker</h1></header>
+}
+
+export default memo(Header)
+
